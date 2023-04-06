23c9453fea59028989cb61d8121f8cc466d20862
Karolis Daugerdas <daugerdas@gmail.com>
2023-04-06 22:01:27 +0300

    add basic countdown functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useEffect, useState, useMemo } from 'react';
+import Countdown from 'react-countdown';
 import './App.css';
 
 function App() {
+  const [session, setSession] = useState("");
+  const [currentMinute, setCurrentMinute] = useState(new Date().getMinutes());
+  const [targetTimeCountdown, setTargetTimeCountdown] = useState();
+
+  useEffect(() => {
+    const interval = setInterval(() => setCurrentMinute(new window.Date().getMinutes()), 1000);
+    
+    let targetTime = new Date();
+
+    if (currentMinute < 25) {
+      targetTime.setMinutes(25);
+      targetTime.setSeconds(0);
+      setSession("work");
+    } else if (currentMinute >= 25 && currentMinute < 30) {
+      targetTime.setMinutes(30);
+      targetTime.setSeconds(0);
+      setSession("break");
+    } else if (currentMinute >= 30 && currentMinute < 55) {
+      targetTime.setMinutes(55);
+      targetTime.setSeconds(0);
+      setSession("work");
+    } else if(currentMinute >= 55){
+      targetTime.setHours(targetTime.getHours() + 1);
+      targetTime.setMinutes(0);
+      targetTime.setSeconds(0);
+      setSession("break");
+    }
+
+    setTargetTimeCountdown(targetTime);
+    
+    return () => {
+      clearInterval(interval);
+    };
+  }, [currentMinute])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Countdown date={targetTimeCountdown} key={targetTimeCountdown} />
+      <div>{session}</div>
     </div>
   );
 }
